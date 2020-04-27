34932968626127801d5c5618e9b7a3f89b4008c4
Esmat Nawahda <esmatnawahda@Esmats-MacBook-Pro.local>
2020-04-27 14:18:05 +0300

    Implemented the first version of the checkout page including the router and the page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import './App.css';
 // Components
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 
 // UTils
@@ -52,11 +54,13 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          
           <Route exact path='/signin' render={() =>
             this.props.currentUser ?
               (<Redirect to='/' />)
               :
-              (<SignInAndSignUp />)} />
+              (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Components
+
+
+// Styles
+import './checkout.styles.scss';
+
+const Checkout = () => {
+    return (
+        <div className='checkout'>
+            Checkout
+        </div>
+    )
+}
+
+export default Checkout;
