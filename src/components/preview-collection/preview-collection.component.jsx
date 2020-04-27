18aa7201b2982d395face1ffa32af69999c50a96
Esmat Nawahda <esmatnawahda@Esmats-MacBook-Pro.local>
2020-04-27 11:03:20 +0300

    implemented addItem redux to be able to add items to the cart

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -12,8 +12,8 @@ const CollectionPreview = ({ title, items }) => (
         </h1>
         <div className='preview'>
             {
-                items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
-                    <CollectionITem key={id} {...otherItemProps} />
+                items.filter((item, idx) => idx < 4).map(item => (
+                    <CollectionITem key={item.id} item={item} />
                 ))
             }
         </div>
